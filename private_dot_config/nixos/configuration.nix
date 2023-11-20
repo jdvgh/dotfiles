1a3198323d0b7f62dd9375af72cc5a81116bcbf4
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2023-11-20 13:06:04 +0100

    feat: Change to using nix-hardware for setup

--- private_dot_config/nixos/configuration.nix
+++ private_dot_config/nixos/configuration.nix
@@ -20,61 +20,20 @@ let
 
  in
 {
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-	vulkan-validation-layers
-    ];
-  };
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-hardware.nvidia.prime = {
-       #	sync.enable = true;
-	# Make sure to use the correct Bus ID values for your system!
-	nvidiaBusId = "PCI:1:0:0";
-  };
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-    ];
+  "${builtins.fetchGit { url = "https://github.com/jdvgh/nixos-hardware.git"; ref = "feat/lenovo-legion-15-ach6";}}/lenovo/legion/15ach6"
+];
 
+    
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.luks.devices."luks-99f1418e-d80c-4bc5-a9d3-3d04b545e0e4".device = "/dev/disk/by-uuid/99f1418e-d80c-4bc5-a9d3-3d04b545e0e4";
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -220,18 +179,20 @@ enableSSHSupport = true;
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-
+ virtualisation.docker.enable = true; 
   users.users.jonase = {
     isNormalUser = true;
     description = "j";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker"];
     packages = with pkgs; [
       alacritty
+      lsof
       bitwarden
       chezmoi
       curl
       dmenu
       docker
+      docker-compose
       firefox
       flameshot
       fzf
@@ -240,10 +201,14 @@ enableSSHSupport = true;
       gnome.gnome-control-center
       gnome.gnome-tweaks
       unstable.go
+      killall
       google-chrome
       go-task
       hcloud
       kubectl
+      goreman
+      krew
+      kubecm
       k9s
       neovim
       pavucontrol
@@ -280,6 +245,8 @@ users.defaultUserShell = pkgs.zsh;
   # $ nix search wget
 
   environment.systemPackages = with pkgs; [
+
+  vscode
 
     unzip
     dig
