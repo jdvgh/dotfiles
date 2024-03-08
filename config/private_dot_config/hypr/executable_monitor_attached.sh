9483688345aef7a936e02a458f290f45bd7a1bf6
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-03-08 20:45:34 +0100

    feat: Remove obsolete position in hypr monitor script

--- config/private_dot_config/hypr/executable_monitor_attached.sh
+++ config/private_dot_config/hypr/executable_monitor_attached.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-hyprctl keyword monitor HDMI-A-1,3440x1440,1920x0,auto
+hyprctl keyword monitor HDMI-A-1,3440x1440,,auto
 
 hyprctl keyword monitor eDP-1,disable
