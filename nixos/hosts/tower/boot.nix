3e1abab33edf692da2de82d3035e04a9d24db9b1
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-03-16 21:39:10 +0100

    feat(nix): Add tower

---
+++ nixos/hosts/tower/boot.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  boot = {
+    loader = {
+	  grub.enable = true;
+	  grub.device = "/dev/sdb";
+	  #grub.device = "/dev/disks/by-id/ata-INTENSO_2FA6079417F500165904";
+	  grub.useOSProber = true;
+      #systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+      };
+    };
+  };
+}
