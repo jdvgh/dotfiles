fdf6e1c76666028fb77e816b9bcc6a5963415108
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-03-21 07:48:02 +0100

    feat(hypr): Add hyprkeys and wofi + mojii

---
+++ config/private_dot_config/hypr/executable_hyprkeys.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eou pipefail
+JSON=$(hyprkeys --from-ctl --json | jq -r --slurp "[.[]][0]")
+USER_SELECTED=$(echo $JSON | jq -r '.[] | "\(.mods) \(.key) \(.dispatcher) \(.arg)"' | wofi --dmenu -p 'Keybinds' --define "dmenu-print_line_num=true");
+
+if [ -z "$USER_SELECTED" ]; then
+    exit 0;
+fi
+
+EVENT=$(echo $JSON | jq -r "[.[]] | .[$USER_SELECTED]" | jq -r '"\(.dispatcher) \(.arg)"');
+
+hyprctl dispatch "$EVENT";
