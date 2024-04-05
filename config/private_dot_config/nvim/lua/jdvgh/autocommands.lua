cc3ac5e3c440f02343f20b137c1498be6b25607a
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-04-05 16:04:13 +0200

    feat(nvim): Add resize

--- config/private_dot_config/nvim/lua/jdvgh/autocommands.lua
+++ config/private_dot_config/nvim/lua/jdvgh/autocommands.lua
@@ -8,3 +8,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd(
+  'VimResized',
+  {
+    group = wr_group,
+    pattern = '*',
+    command = 'wincmd =',
+    desc = 'Automatically resize windows when the host window size changes.'
+  }
+)
