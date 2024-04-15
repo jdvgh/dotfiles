1debf3764f3cc478d094c726c201c68890a9ef58
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-04-15 18:20:02 +0200

    feat(git): Add tower setup for git and hyprland fix

--- config/private_dot_config/hypr/executable_monitor_attached.sh
+++ config/private_dot_config/hypr/executable_monitor_attached.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-hyprctl keyword monitor HDMI-A-1,3440x1440,auto,auto
-hyprctl keyword monitor HDMI-A-4,3440x1440,auto,auto
+# hyprctl keyword monitor HDMI-A-1,3440x1440,auto,auto
+hyprctl keyword monitor HDMI-A-5,3440x1440,auto,auto
+# hyprctl keyword monitor HDMI-A-4,1920x1080,auto,auto
 
 hyprctl keyword monitor eDP-1,disable
