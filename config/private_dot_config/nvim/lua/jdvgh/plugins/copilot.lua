17d7429732ba255d1f1a28a53f12790c71b4c554
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-05-28 17:53:25 +0200

    feat(nvim): Add copilot

--- config/private_dot_config/nvim/lua/jdvgh/plugins/copilot.lua
+++ config/private_dot_config/nvim/lua/jdvgh/plugins/copilot.lua
@@ -1,8 +1,8 @@
 return {
   {
     'zbirenbaum/copilot.lua',
-    cmd = "Copilot",
-    event = "InsertEnter",
+    -- cmd = "Copilot",
+    -- event = "InsertEnter",
     config = function()
       require("copilot").setup({
         suggestion = { enabled = false },
