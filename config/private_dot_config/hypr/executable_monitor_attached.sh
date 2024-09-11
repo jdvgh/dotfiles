af73ef542a40737c28884d70ff260255cec5f610
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-09-11 12:08:00 +0200

    feat(chezmoi): Fix hypr

--- config/private_dot_config/hypr/executable_monitor_attached.sh
+++ config/private_dot_config/hypr/executable_monitor_attached.sh
@@ -3,6 +3,5 @@ set -eou pipefail
 
 # hyprctl keyword monitor HDMI-A-1,3440x1440,auto,auto
 hyprctl keyword monitor HDMI-A-5,3440x1440,auto,auto
-# hyprctl keyword monitor HDMI-A-5,1920x1080,auto,auto
 
 hyprctl keyword monitor eDP-1,disable
